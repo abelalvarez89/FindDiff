653046c64b0be7c0b7b215e32746bd42552c2c54
abelalvarez89 <abelalvarez89@hotmail.com>
2015-06-05 17:16:00 -0400

    Find Differences Project

---
+++ FindDifference/FindDifference/Form1.Designer.cs
@@ -0,0 +1,403 @@
+﻿namespace FileDialog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input1 = new System.Windows.Forms.TextBox();
+            this.input1Label = new System.Windows.Forms.Label();
+            this.input2Label = new System.Windows.Forms.Label();
+            this.input2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.newLineCount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.repetition = new System.Windows.Forms.CheckBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // input1
+            // 
+            this.input1.AcceptsReturn = true;
+            this.input1.AcceptsTab = true;
+            this.input1.AllowDrop = true;
+            this.input1.Location = new System.Drawing.Point(20, 295);
+            this.input1.Multiline = true;
+            this.input1.Name = "input1";
+            this.input1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.input1.Size = new System.Drawing.Size(231, 220);
+            this.input1.TabIndex = 0;
+            this.input1.TextChanged += new System.EventHandler(this.input1_TextChanged);
+            // 
+            // input1Label
+            // 
+            this.input1Label.AutoSize = true;
+            this.input1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input1Label.Location = new System.Drawing.Point(99, 266);
+            this.input1Label.Name = "input1Label";
+            this.input1Label.Size = new System.Drawing.Size(85, 26);
+            this.input1Label.TabIndex = 2;
+            this.input1Label.Text = "Input 1";
+            this.input1Label.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // input2Label
+            // 
+            this.input2Label.AutoSize = true;
+            this.input2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input2Label.Location = new System.Drawing.Point(624, 264);
+            this.input2Label.Name = "input2Label";
+            this.input2Label.Size = new System.Drawing.Size(85, 26);
+            this.input2Label.TabIndex = 4;
+            this.input2Label.Text = "Input 2";
+            // 
+            // input2
+            // 
+            this.input2.AllowDrop = true;
+            this.input2.Location = new System.Drawing.Point(545, 293);
+            this.input2.Multiline = true;
+            this.input2.Name = "input2";
+            this.input2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.input2.Size = new System.Drawing.Size(231, 220);
+            this.input2.TabIndex = 3;
+            this.input2.TextChanged += new System.EventHandler(this.input2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(280, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 34);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Compare";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.compareButton_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(280, 295);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(231, 220);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.outputSame_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.AcceptsReturn = true;
+            this.textBox4.AcceptsTab = true;
+            this.textBox4.Location = new System.Drawing.Point(20, 47);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox4.Size = new System.Drawing.Size(231, 202);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextChanged += new System.EventHandler(this.output1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(353, 267);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 26);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Same";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(66, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 26);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Input 1 Diff";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(594, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 26);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Input 2 Diff";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(545, 42);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox5.Size = new System.Drawing.Size(231, 202);
+            this.textBox5.TabIndex = 12;
+            this.textBox5.TextChanged += new System.EventHandler(this.output2_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(221, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(748, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(433, 276);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(190, 276);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(715, 277);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(276, 47);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(119, 20);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Output Style:";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(281, 76);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(75, 13);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Surround with:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(281, 120);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(81, 13);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "Separate using:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.AcceptsReturn = true;
+            this.textBox6.AcceptsTab = true;
+            this.textBox6.Location = new System.Drawing.Point(285, 92);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(25, 20);
+            this.textBox6.TabIndex = 27;
+            this.textBox6.TextChanged += new System.EventHandler(this.surroundLeft_TextChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.AcceptsReturn = true;
+            this.textBox7.AcceptsTab = true;
+            this.textBox7.Location = new System.Drawing.Point(285, 136);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(62, 20);
+            this.textBox7.TabIndex = 28;
+            this.textBox7.TextChanged += new System.EventHandler(this.separation_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.AcceptsReturn = true;
+            this.textBox8.AcceptsTab = true;
+            this.textBox8.Location = new System.Drawing.Point(322, 92);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(25, 20);
+            this.textBox8.TabIndex = 29;
+            this.textBox8.TextChanged += new System.EventHandler(this.surroundRight_TextChanged);
+            // 
+            // newLineCount
+            // 
+            this.newLineCount.Location = new System.Drawing.Point(429, 175);
+            this.newLineCount.Name = "newLineCount";
+            this.newLineCount.Size = new System.Drawing.Size(23, 20);
+            this.newLineCount.TabIndex = 31;
+            this.newLineCount.TextChanged += new System.EventHandler(this.newLineCount_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(426, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "Columns per row";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(458, 173);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(62, 23);
+            this.button3.TabIndex = 33;
+            this.button3.Text = "Export";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.saveFileDialog_Click);
+            // 
+            // repetition
+            // 
+            this.repetition.AutoSize = true;
+            this.repetition.Location = new System.Drawing.Point(285, 189);
+            this.repetition.Name = "repetition";
+            this.repetition.Size = new System.Drawing.Size(107, 17);
+            this.repetition.TabIndex = 34;
+            this.repetition.Text = "Ignore Repetition";
+            this.repetition.UseVisualStyleBackColor = true;
+            this.repetition.CheckedChanged += new System.EventHandler(this.repetition_CheckedChanged);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(445, 223);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 35;
+            this.clearButton.Text = "Clear All";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 541);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.repetition);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.newLineCount);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.input2Label);
+            this.Controls.Add(this.input2);
+            this.Controls.Add(this.input1Label);
+            this.Controls.Add(this.input1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input1;
+        private System.Windows.Forms.Label input1Label;
+        private System.Windows.Forms.Label input2Label;
+        private System.Windows.Forms.TextBox input2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox newLineCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox repetition;
+        private System.Windows.Forms.Button clearButton;
+
+    }
+}
+
---
+++ FindDifference/FindDifference/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileDialog
+{
+    public partial class Form1 : Form
+    {
+        private string _surroundLeft = "";
+        private string _surroundRight = "";
+        private string _separation = "";
+        private static bool _ignoreRepetition = true;
+        private static string inputTemp1 = "";
+        private static string inputTemp2 = "";
+
+        private int lineCount;
+
+        private static List<string> _list1 = new List<string>();
+        private static List<string> _list2 = new List<string>();
+        private static List<string> _mutual = new List<string>();
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            repetition.Checked = true;
+        }
+
+        private void input1_TextChanged(object sender, EventArgs e)
+        {
+            inputTemp1 = input1.Text;
+        }
+
+        private void input2_TextChanged(object sender, EventArgs e)
+        {
+            inputTemp2 = input2.Text;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private static void CleanLists()
+        {
+            _list1 = new List<string>();
+            _list2 = new List<string>();
+            _mutual = new List<string>();
+        }
+
+        private void compareButton_Click(object sender, EventArgs e)
+        {
+            CleanLists();
+             
+            var str1 = inputTemp1.Split(default(string[]), StringSplitOptions.RemoveEmptyEntries);
+            var str2 = inputTemp2.Split(default(string[]), StringSplitOptions.RemoveEmptyEntries);
+
+            var tempList1 = ConvertToList(str1);
+            var tempList2 = ConvertToList(str2);
+
+            label8.Text = tempList1.Count.ToString();
+            label9.Text = tempList2.Count.ToString();
+
+            CopyList(tempList1, _list1);
+            CopyList(tempList2, _list2);
+
+            FindMutual(tempList1, tempList2);
+           
+            
+            
+            OutputResults();
+
+        }
+
+        public void OutputResults()
+        {
+            textBox3.Text = CastToString(_mutual, lineCount);
+            label7.Text = _mutual.Count.ToString();
+            textBox4.Text = CastToString(_list1, lineCount);
+            label5.Text = _list1.Count.ToString();
+            textBox5.Text = CastToString(_list2, lineCount);
+            label6.Text = _list2.Count.ToString();
+        }
+
+        private string CastToString(IEnumerable<string> list, int newLineCount)
+        {
+            var returnValue = "";
+            int i = 1;
+            foreach (var value in list)
+            {
+
+                returnValue += _surroundLeft + value + _surroundRight + _separation;
+                if (newLineCount > 0 && i % newLineCount == 0)
+                {
+                    returnValue += "\r\n";
+                }
+                i++;
+            }
+            return returnValue;
+        }
+
+        private static List<string> ConvertToList(IEnumerable<string> arr)
+        {
+            return arr.ToList();
+        }
+
+        private static void CopyList(IEnumerable<string> fromList, ICollection<string> destination)
+        {
+            foreach (var temp in fromList)
+            {
+                destination.Add(temp);
+            }
+        }
+
+        private static void FindMutual(IEnumerable<string> temp1, List<string> temp2)
+        {
+            var selectedItem1 = new int[_list1.Count];
+            var selectedItem2 = new int[_list2.Count];
+            var i = 0;
+            foreach (var list1Temp in temp1)
+            {
+                var j = 0;
+                foreach (var list2Temp in temp2)
+                {
+                    if (list1Temp.Equals(list2Temp, StringComparison.OrdinalIgnoreCase))
+                    {
+                        if ((selectedItem1[i] == 1 || selectedItem2[j] == 1) && _ignoreRepetition)
+                        {
+                            j++;
+                            continue;
+                        }
+                        selectedItem1[i] = 1;
+                        selectedItem2[j] = 1;
+                        _mutual.Add(list2Temp);
+                        _list1.Remove(list1Temp);
+                        _list2.Remove(list2Temp);
+                    }
+                    j++;
+                }
+                i++;
+            }
+        }
+
+        private void surroundLeft_TextChanged(object sender, EventArgs e)
+        {
+            _surroundLeft = textBox6.Text;
+        }
+
+        private void separation_TextChanged(object sender, EventArgs e)
+        {
+            _separation = textBox7.Text;
+        }
+
+        private void surroundRight_TextChanged(object sender, EventArgs e)
+        {
+            _surroundRight = textBox8.Text;
+        }
+
+        private void output1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void output2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void outputSame_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newLineCount_TextChanged(object sender, EventArgs e)
+        {
+            var numVal = 0;
+            try
+            {
+                numVal = Convert.ToInt32(newLineCount.Text);
+            }
+            catch (Exception)
+            {
+                numVal = 0;
+            }
+            lineCount = numVal;
+        }
+
+        private void saveFileDialog_Click(object sender, EventArgs e)
+        {
+            var saveFileDialog1 = new SaveFileDialog();
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (var s = File.Open(saveFileDialog1.FileName, FileMode.Create))
+                using (var sw = new StreamWriter(s))
+                {
+                    var outputFile = "========Mutual=========\r\n\r\n";
+                    outputFile += CastToString(_mutual, lineCount);
+                    outputFile += "\r\n\r\n========";
+                    outputFile += ("Output1");
+                    outputFile += ("========\r\n\r\n\r\n");
+                    outputFile += (CastToString(_list1, lineCount));
+                    outputFile += ("\r\n\r\n========");
+                    outputFile += ("Output2");
+                    outputFile += ("========\r\n\r\n\r\n");
+                    outputFile += (CastToString(_list2, lineCount));
+
+                    sw.Write(outputFile);
+                }
+            }
+        }
+
+        private void repetition_CheckedChanged(object sender, EventArgs e)
+        {
+            _ignoreRepetition = !_ignoreRepetition;
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            input1.Text = "";
+            input2.Text = "";
+        }
+    }
+}
